2944db6d91a1bcdfd7ae565c642538a3b9508ac4
plejeail <pierre.lejeail@hotmail.fr>
2017-02-13 16:21:04 +0100

    MSongDB

---
+++ pytest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 15:18:29 2017
+
+@author: etu2016
+"""
+
+import MSongDB.hdf5_getters as nimportequoituvavoir
+
+nimportequoituvavoir.open_h5_file_read("TRAXLZU12903D05F94.h5")
