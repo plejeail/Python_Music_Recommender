71a75989474eb7f0a1725f225d9b39561ef8e390
Yongwe Jean-Luc <yongwe52@gmail.com>
2017-02-21 14:17:12 +0100

    Update fingerprints_export.py

--- package/tools/fingerprints_export.py
+++ package/tools/fingerprints_export.py
@@ -9,12 +9,13 @@ path_audios = 'genres/genres_converted/test_files'
 
 def fingerprints_exports(path_files):
     list_fingerprints = []
-   # dataframe_fingerprints = pd.DataFrame()
+
     for files in os.listdir(path_files):
         audio = path_files + "/" + files
+        #storage of the audios files as 'audio.wav'
         list_fingerprints.append(fingerprinter(audio))
         
-        
+        #sotrage as a dataframe (to ease the exportation)
     dataframe_fingerprints = pd.DataFrame(list_fingerprints)
     dataframe_fingerprints.columns = ['fingerprints']
     
@@ -25,5 +26,6 @@ def fingerprints_exports(path_files):
 def storage_fingerprints(dataframe_fingerprints):
     dataframe_fingerprints.to_csv('audios_fingerprints.csv' , sep = ';')
 
+    #functions calls
 fingerprints_audios = fingerprints_exports(path_audios)
 storage_fingerprints(fingerprints_audios)
