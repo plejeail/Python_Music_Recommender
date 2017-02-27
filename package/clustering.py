c5d0968f9dfb53e640d0f681fe7cd12dda0bf7a5
plejeail <pierre.lejeail@hotmail.fr>
2017-02-27 21:16:00 +0100

    WTF

--- package/clustering.py
+++ package/clustering.py
@@ -10,45 +10,41 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn import preprocessing
 
 
-# %% Importation and first treatments
+# %% Importation and first treatments ----------------------------------------
 with open("db/h5.csv", 'r') as csvfile:
     table = pnd.DataFrame(pnd.read_csv(csvfile, sep=';'))
 
-# index=range(0,10000,1)
 numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 dataset = table.select_dtypes(include=numerics)
 
 # Select Lebron James from our dataset
-selected_player = dataset.loc[1]
+selected_plajyer = dataset.loc[1]
 
+# Variables description
+dataset.describe()
+# variance(asr) = 0 and variance(energy) = 0 so we remove them
+del dataset['asr']
+del dataset['energy']
+dataset.fit_transform()
+
+# ?? We didn't find Nan but this seems necessary
+dataset = dataset.fillna(dataset.mean(), inplace=True)
+
+# Correlations between lines
+xcorr = np.corrcoef(dataset)
+xcorr = xcorr[np.logical_not(np.isnan(xcorr))]
+xcorr.min()
+
+# Correlations between columns
+ycorr = np.corrcoef(dataset, rowvar=0)
+ycorr = ycorr[np.logical_not(np.isnan(ycorr))]
+ycorr.min()
+
+# We scale columns so they will be comparable
+df_scaled = preprocessing.scale(dataset)
+
+# %% knn ---------------------------------------------------------------------
 # Find the distance from each player in the dataset to lebron.
 # lebron_distance = dfnba.apply(euclidean_distance, axis=1)
-
 knn = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(dataset)
 knn.kneighbors(dataset)
-
-# BUG SEARCH ---------------------------------------------------------------- #
-# Input contains NaN, infinity or a value too large for dtype('float64').
-# Check na for all variables
-np.isnan(np.sum(dataset))
-# Check infinite values for all variables
-np.isinf(np.sum(dataset))
-# value to large ???
-dataset2 = np.round(dataset, 2)
-knn = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(dataset2)
-# no
-# Try with scaled variables
-df_scaled = preprocessing.scale(dataset)
-# same problem
-for i in range(dataset.shape[1]):
-    preprocessing.scale(dataset.iloc[:, i])
-# variable by variable doesn't seems to cause problems
-for i in range(dataset.shape[1]):
-    try:
-        preprocessing.scale(dataset.iloc[:, -i])
-    except:
-        print(i)
-# problems when we remove 12, 13, 14, 15 but not the others
-# works whithout year
-preprocessing.scale(dataset.iloc[:, -1])
-# Therefore, I will continue without year
